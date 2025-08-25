7fa753cac1d5a8644e4dd47a2528045b295a25d8
Surya <surya.dev.singh@thoughtclan.com>
2025-08-25 17:01:03 +0530

    second commit

--- src/main/java/com/example/mavn_proj/MavnProjApplication.java
+++ src/main/java/com/example/mavn_proj/MavnProjApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MavnProjApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MavnProjApplication.class, args);
-	}
 
+		SpringApplication.run(MavnProjApplication.class, args);
+		System.out.println("hello");
+	}
 }
